fb6743a13ebee90b0b29e3b097ba62e750f91333
Олег Башкин <ol.bashkin@gmail.com>
2020-06-15 00:45:59 +0300

    terms and privacy

--- frontend/markup/components/head/data/data.js
+++ frontend/markup/components/head/data/data.js
@@ -2,14 +2,26 @@ data = {
     head: {
         rest: {
             title: 'Foody — end-to-end order management platform for restaurants and suppliers',
-            description: "Foody — end-to-end order management platform for food service industry. We provide restaurants with one window for  placing orders, scanning invoices, making payments to their suppliers. All from one app.",
+            description: "We provide restaurants with one window for placing orders, scanning invoices, making payments to their suppliers. All from one app.",
             share: "rest",
             useSocialMetaTags: true
         },
         sup: {
             title: 'Foody — end-to-end order management platform for restaurants and suppliers',
-            description: "Foody — end-to-end order management platform for food service industry. We provide suppliers with one window for  receiving orders from your current clients and ACH payments, getting new clients. All from one app.",
+            description: "We provide suppliers with one window for receiving orders from your current clients and ACH payments, getting new clients. All from one app.",
             share: "sup",
+            useSocialMetaTags: true
+        },
+        privacy: {
+            title: 'Foody — end-to-end order management platform for restaurants and suppliers',
+            description: "We provide restaurants and suppliers with one window for placing and receiving orders, scanning invoices, making payments and getting new clients. All from one app.",
+            share: "rest",
+            useSocialMetaTags: true
+        },
+        terms: {
+            title: 'Foody — end-to-end order management platform for restaurants and suppliers',
+            description: "We provide restaurants and suppliers with one window for placing and receiving orders, scanning invoices, making payments and getting new clients. All from one app.",
+            share: "rest",
             useSocialMetaTags: true
         }
     }
--- frontend/markup/components/nav/data/data.js
+++ frontend/markup/components/nav/data/data.js
@@ -1,16 +1,25 @@
 data = {
     nav: {
         rest: {
+            isterms: false,
             isrest: true,
             issup: false,
             restlink: "",
             suplink: "suppliers.html"
         },
         sup: {
+            isterms: false,
             isrest: false,
             issup: true,
             restlink: "./",
             suplink: ""
+        },
+        terms: {
+            isterms: true,
+            isrest: false,
+            issup: false,
+            restlink: "../",
+            suplink: "../suppliers.html"
         }
     }
 }
